aaad4897d2538c4573369d13097a942c9ca873fa
Pol Valle <doplax@gmail.com>
2022-08-30 09:51:28 +0200

    Creados los 4 Contenedores, alienados los divs: 	texto + caja 	texto + caja 	texto + caja

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
 
-let $content = document.getElementById("content");
-let optionChoice = document.getElementById("option-choice");
-let $btn = document.getElementById("btn");
-// console.log(optionChoice);
-
-optionChoice.addEventListener("change",function(){
-    
-    optionChoice = document.getElementById("option-choice");
-    let newElement = document.createElement("h1");
-    newElement.innerText = optionChoice.value
-    $content.insertBefore($btn ,newElement[0]);
-    
-})
 
 
 
