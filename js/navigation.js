23604827cb92e8736d514fd5be25cb429a7fa7fb
Doplax <doplax@gmail.com>
2023-04-05 10:16:11 +0200

    He unificado todo el HTML en un mismo fichero y he implementado la logica de "inactive"

---
+++ js/navigation.js
@@ -0,0 +1,32 @@
+const contCentradores = document.getElementById('container-centrador')
+const contElectrodos  = document.getElementById('container-electrodo')
+const contPerno       = document.getElementById('container-perno')
+
+
+// CAMBIAR Sección
+const btn_centrador = document.getElementById("btn_centrador");
+btn_centrador.addEventListener("click", () => {
+    contCentradores.classList.remove('inactive');
+    contElectrodos.classList.add('inactive');
+    contPerno.classList.add('inactive');
+});
+
+const btn_electrodo = document.getElementById("btn_electrodo");
+btn_electrodo.addEventListener("click", () => {
+    contCentradores.classList.add('inactive');
+    contElectrodos.classList.remove('inactive');
+    contPerno.classList.add('inactive');
+});
+
+const btn_perno= document.getElementById("btn_perno");
+btn_perno.addEventListener("click", () => {
+    contCentradores.classList.add('inactive');
+    contElectrodos.classList.add('inactive');
+    contPerno.classList.remove('inactive');
+});
+
+
+
+
+
+
